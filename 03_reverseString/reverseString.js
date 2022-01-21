d2b84660939ac106891cb7590d04b92c0de9044d
caroline <carzhang96@gmail.com>
2022-01-21 12:23:39 -0500

    add the solutions to reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+    let strReversed = '';
+    for (let i = str.length-1; i >= 0; i--){
+        strReversed += str.charAt(i);
+    }
+    return strReversed;
 };
 
 // Do not edit below this line
