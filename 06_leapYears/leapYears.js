9cb1bfd89c34461c5f2505317d3af0389804d911
caroline <carzhang96@gmail.com>
2022-01-21 12:27:19 -0500

    add the solutions to leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //if divisible by 400 or divisible by 4 and !divisible by 100
+    return year%400 == 0 || (year%4 == 0 && !(year%100 == 0));
 };
 
 // Do not edit below this line
