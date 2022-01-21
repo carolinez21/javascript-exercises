81c0cf25a37315d4871870b9ffdeb7787b9b6c23
caroline <carzhang96@gmail.com>
2022-01-21 12:29:16 -0500

    add the solutions to palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = str.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    let strArray = newStr.split("");
+    let reverseArray = strArray.reverse();
+    let reverseStr = reverseArray.join("");
+    if(newStr === reverseStr) return true;
+    else return false;
 };
 
 // Do not edit below this line
